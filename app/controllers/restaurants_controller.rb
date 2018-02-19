c9095a63427ca377b8fb526cb656dc202261989e
Ed <ea.litten@gmail.com>
2018-02-19 15:27:11 +0000

    Add restaurant list view
    Co-authored-by: rcvink <rrvink@gmail.com>
    Co-authored-by: philb56 <phil.bugden@outlook.com>
    Co-authored-by: attyc <antoniajcronin@gmail.com>

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,24 @@
 class RestaurantsController < ApplicationController
+   # GET '/restaurants'
   def index
+    @restaurants = Restaurant.all
   end
 
+  # GET '/restaurants/id'
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
+  # GET '/restaurants/new'
   def new
   end
 
+  # POST '/restaurants'
   def create 
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to @restaurant
   end
-
 
   private
 
