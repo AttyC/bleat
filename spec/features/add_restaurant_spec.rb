c81ff138b26c331e64c7b93889811ce0f6b1bc45
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-20 12:58:12 +0000

    Add tests for create restaurant
    Co-authored-by: rcvink <rrvink@gmail.com>
    Co-authored-by: philb56 <phil.bugden@outlook.com>
    Co-authored-by: ealitten <ealitten@gmail.com>
    Co-authored-by: alexse5121 <alexse5121@gmail.com>

---
+++ spec/features/add_restaurant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Add restaurant", :type => :feature do
+  feature 'Create restaurant' do
+    scenario "displays restaurant in list after creating it" do
+      visit '/restaurants'
+      click_on 'New restaurant'
+      fill_in 'restaurant[name]', with: 'Testaurant'
+      fill_in 'restaurant[description]', with: 'Delicious'
+      click_button 'Create Restaurant'
+      expect(page).to have_content('Testaurant')
+      expect(page).to have_content('Delicious')
+    end
+  end
+end
---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Restaurant, type: :model do
+  context 'initially' do
+    it 'has a name' do
+      restaurant = Restaurant.create(name: "Testaurant", description: "Tasty tests")
+      expect(Restaurant.find_by(name: "Testaurant").name).to eq restaurant.name
+    end
+
+    it 'has a description' do
+      restaurant = Restaurant.create(name: "Testaurant", description: "Tasty tests")
+      expect(Restaurant.find_by(description: "Tasty tests").description).to eq restaurant.description
+    end
+  end
+end
